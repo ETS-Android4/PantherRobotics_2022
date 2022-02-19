7b3f8c47f798c5a3c91b1cf5d159c0bb972011b0
HerveSV <herve.schmitveiler@icloud.com>
2022-02-19 09:11:23 +0800

    hjh

--- TeamA/src/main/java/org/firstinspires/ftc/teamcode/testAuton.java
+++ TeamA/src/main/java/org/firstinspires/ftc/teamcode/testAuton.java
@@ -22,7 +22,11 @@ public class testAuton extends AlphaPantherOp {
         telemetry.addData("Moving lift", 50);
         telemetry.update();
         while(lift.isBusy()) {}
+        telemetry.addData("Thing 1", 0);
+        telemetry.update();
         lift.setPower(0);
+        telemetry.addData("Thing 2", 0);
+        telemetry.update();
         tankDrive(500, 0.5, false);
 
     }
